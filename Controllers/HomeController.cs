3ca6b6ac27d958a8b857b9fcfe0ac22f1f6e5a05
ubiracy <ubiracy@exemple.com>
2023-07-28 09:40:59 -0300

    lhpet commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,71 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using LHPet.Models;
+
+namespace LHPet.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index() 
+
+    {
+        //Instancias do tipo Cliente
+
+        Cliente cliente1 = new Cliente(01, "Arthur A. Ferreira", "857.032.950-41", "arthur.antunes@sp.senai.br", "Madruga");
+        Cliente cliente2 = new Cliente(02, "William Henry Gates III","039.618.250-09", "bill@microsoft.com","Bug" );
+        Cliente cliente3 = new Cliente(03, "Ada Lovelace", "800.777.920-50", "ada@adalanguage.com", "Byron");
+        Cliente cliente4 = new Cliente(04,"Linus Torvalds", "935.622.400-03", "torvalds@osdl.org", "Pinguim");
+        Cliente cliente5 = new Cliente(05, "Grace Hooper", "911.702.988-00", "grace.hooper@cobol.com", "Loboc");
+
+        
+        //lista de Clientes e Atribui os clientes
+        List<Cliente> listaClientes = new List<Cliente>();
+        listaClientes.Add(cliente1);
+        listaClientes.Add(cliente2);
+        listaClientes.Add(cliente3);
+        listaClientes.Add(cliente4);
+        listaClientes.Add(cliente5);
+
+        ViewBag.listaClientes = listaClientes;
+
+        
+        //Instacias do tipo Fornecedores e Atribui os Fornecedores
+        Fornecedor fornecedor1 = new Fornecedor(01, "C# PET S/A", "14.182.102/0001-80", "c-sharp@pet.org");
+        Fornecedor fornecedor2 = new Fornecedor(02, "Ctrl Alt Dog", "15.836.698/0001-57", "ctrl@alt.dog.br");
+        Fornecedor fornecedor3 = new Fornecedor(03, "BootsPet INC", "40.810.224/0001-09", "boots.pet@gatomania.us");
+        Fornecedor fornecedor4 = new Fornecedor(04, "Tik Tok Dogs", "87.945.350/0001-09", "nosmamidia@tiktokdogs.uk");
+        Fornecedor fornecedor5 = new Fornecedor(05, "Bifinho Forever", "18.760.614/0001-37", "contato@bff.us");
+
+
+        //Lista de Fornecedores
+        List<Fornecedor> ListaFornecedores = new List<Fornecedor>();
+        ListaFornecedores.Add(fornecedor1);
+        ListaFornecedores.Add(fornecedor2);
+        ListaFornecedores.Add(fornecedor3);
+        ListaFornecedores.Add(fornecedor4);
+        ListaFornecedores.Add(fornecedor5);
+
+        ViewBag.listaFornecedores = ListaFornecedores;
+                
+
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
